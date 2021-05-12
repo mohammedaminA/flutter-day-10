812cc0eb2cdb154f051415fc7db3461b6f2d6f2d
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-13 02:32:36 +0300

    Refactor

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/utilities/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = 'welcome_screen';
@@ -69,32 +70,3 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
   }
 }
 
-class RoundedButton extends StatelessWidget {
-final String text;
-final Color color;
-final String routeName;
-RoundedButton({@required this.text, @required this.color, @required this.routeName});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        elevation: 5.0,
-        color: color,
-        borderRadius: BorderRadius.circular(30.0),
-        child: MaterialButton(
-          onPressed: () {
-            //Go to login screen.
-            Navigator.pushNamed(context, routeName);
-          },
-          minWidth: 200.0,
-          height: 42.0,
-          child: Text(
-            text,
-          ),
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/utilities/rounded_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final Color color;
+  final String routeName;
+  RoundedButton({@required this.text, @required this.color, @required this.routeName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: () {
+            //Go to login screen.
+            Navigator.pushNamed(context, routeName);
+          },
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            text,
+          ),
+        ),
+      ),
+    );
+  }
+}
