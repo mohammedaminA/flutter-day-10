f67554c63725e3f6224fdc328e12938d33e64b01
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-14 21:32:26 +0300

    Message Bubble

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -157,7 +157,11 @@ class MessageBubble extends StatelessWidget {
             color: Colors.black54
           ),),
           Material(
-            borderRadius: BorderRadius.circular(30.0),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              bottomLeft: Radius.circular(30.0),
+              bottomRight: Radius.circular(30.0)
+            ),
             elevation: 5.0,
             color: Colors.lightBlueAccent,
             child: Padding(
